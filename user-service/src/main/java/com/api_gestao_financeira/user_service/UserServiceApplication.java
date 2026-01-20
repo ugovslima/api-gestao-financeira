0e58c3a851af91c152a3f55faf213efe1a783069
ugovslima <ugovslima@gmail.com>
2026-01-20 10:37:07 -0300

    first commit

---
+++ user-service/src/main/java/com/api_gestao_financeira/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api_gestao_financeira.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/api_gestao_financeira/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_gestao_financeira.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
